0f266dab3490a45964f8b09fc68c9e8b0fba9d1a
Giuseppe <giuseppedf8711@gmail.com>
2020-08-03 16:46:27 +0200

    fine

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+var nome = prompt("inserisci il tuo nome");
+
+var cognome = prompt("inserisci il tuo cognome");
+
+var colore = prompt("inserisci il tuo colore preferito");
+
+var numero = "19";
+
+
+document.getElementById("text").innerHTML = nome + cognome + colore + numero;
