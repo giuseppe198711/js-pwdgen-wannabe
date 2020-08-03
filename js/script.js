a89b0f5645d1fe2b3b444490e9280aff5085edfe
Giuseppe <giuseppedf8711@gmail.com>
2020-08-03 16:55:23 +0200

    aggiunta commenti

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-
+// richiede all'utente il nome
 var nome = prompt("inserisci il tuo nome");
 
+// richiede all'utente il cognome
 var cognome = prompt("inserisci il tuo cognome");
 
+// richiede all'utente il colore preferito
 var colore = prompt("inserisci il tuo colore preferito");
 
+// dichiarazione variabile
 var numero = "19";
 
-
+// stampa a video i precedenti dati richiesti + variabile
 document.getElementById("text").innerHTML = nome + cognome + colore + numero;
